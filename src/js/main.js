2e0d8486a9e726ae84e874ce5ccc45f9326011f5
DiMaNacho <dimanachom@gmail.com>
2015-12-15 03:27:22 -0300

    Funcionando simpleWeather

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,22 @@
+$(appStart);
+
 function appStart() {
-	console.log('Todo loco!');
+	var $tiempo = $("#tiempo");
+
+	$.simpleWeather({
+		location: 'Montevideo, UY',
+		woeid: '',
+		unit: 'c',
+		success: function(weather) {
+			html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+			html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+			html += '<li class="currently">'+weather.currently+'</li>';
+			html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+
+			$tiempo.html(html);
+		},
+		error: function(error) {
+			$tiempo.html('<p>'+error+'</p>');
+		}
+	});
 }
-$(document).on('ready', appStart);
