d09bccfea09d34281548dcdfb1d8a3511c89851e
DiMaNacho <dimanacho@obrusly.com>
2015-12-16 18:44:10 -0300

    Test para heroku php

---
+++ test.php
@@ -0,0 +1,33 @@
+<!DOCTYPE HTML>
+<!--[if lt IE 7]><html class="lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]><html class="lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]><html class="lt-ie9" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--><html lang="es"><!--<![endif]-->
+<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>El Tiempo - ScreenMedia</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="css/main.min.css">
+		
+		<!-- Google Analytics -->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-XXXXX-X', 'auto');
+			ga('send', 'pageview');
+		</script>
+</head>
+<body>
+	<h1 class="titulo">El tiempo te lo trae <img src="./img/logo-marca.png" width="300"></h1>
+	<article class="tiempo"><ul></ul></article>
+
+	<!-- JS -->
+	<script src="js/main.min.js"></script>
+</body>
+</html>
